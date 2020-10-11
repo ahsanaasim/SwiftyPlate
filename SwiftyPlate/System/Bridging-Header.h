ef49b7f21da74a011016f584775c58f95fc989ef
Jamil Bin Hossain <jamil@6sensehq.com>
2020-10-11 20:06:39 +0600

    Restructure & Add RSwift

---
+++ SwiftyPlate/System/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  SwiftyPlate
+//
+//  Created by AKM Ahsanuzzaman on 11/10/20.
+//  Copyright © 2020 6sense Technologies. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
